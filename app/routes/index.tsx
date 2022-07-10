58da4c0019d7abe03c95988781c05231ccd723ab
Josh Marantz <josh@marantz.dev>
2022-07-10 17:16:14 -0400

    add actually interesting code

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,41 @@
+import { DataFunctionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+
+export async function loader({ request }: DataFunctionArgs) {
+  return Object.fromEntries(new URL(request.url).searchParams);
+}
+
 export default function Index() {
+  const data = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+      <h1>Form shtuffs</h1>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Form id="someFormId">
+          <label htmlFor="search">
+            Search:&nbsp;
+            <input
+              defaultValue={data.search}
+              id="search"
+              name="search"
+              type="search"
+            />
+          </label>
+        </Form>
+        <label htmlFor="sort">
+          Sort by:&nbsp;
+          <select
+            name="sort"
+            id="sort"
+            defaultValue={data.sort}
+            form="someFormId"
+            onChange={(e) => e.target.form?.requestSubmit()}
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            <option>A-Z</option>
+            <option>Z-A</option>
+          </select>
+        </label>
+      </div>
     </div>
   );
 }
